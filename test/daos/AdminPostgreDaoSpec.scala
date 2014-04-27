3119e2f39c622904dddbccc8470c78a70a93260b
ybr <ybr@zenexity.com>
2014-04-27 17:22:26 +0200

    [test] complete admin tests

--- test/daos/AdminPostgreDaoSpec.scala
+++ test/daos/AdminPostgreDaoSpec.scala
@@ -2,39 +2,36 @@ package daos
 
 import scala.concurrent._
 
-import org.joda.time._
-
 import org.specs2.mutable._
 
 import models.requests._
 import models.exceptions._
 
-// TODO complete tests
 object AdminPostgreDaoSpec extends Specification with DaoSpec {
   "AdminPostgreDAO" should {
     "create an account" in new WithInMemoryDB {
-      val admin = await(AdminPostgreDAO.create(AdminCreate("firstName", "lastName", "admin@domain.com", DateTime.now), "login", "pwd", "someSalt"))
+      val admin = await(AdminPostgreDAO.create(Fixtures.adminCreate0, "login", "pwd", "someSalt"))
       admin.id.value.length > 0 must beTrue
     }
 
     "not create an account when the login already exists" in new WithInMemoryDB {
       await {
-        AdminPostgreDAO.create(AdminCreate("firstName0", "lastName0", "admin@domain.com", DateTime.now),"login",  "pwd", "someSalt")
+        AdminPostgreDAO.create(Fixtures.adminCreate0, "login",  "pwd", "someSalt")
       }
       await {
-        AdminPostgreDAO.create(AdminCreate("firstName1", "lastName1", "admin1@domain.com", DateTime.now),"login",  "otherPwd", "someOtherSalt")
+        AdminPostgreDAO.create(Fixtures.adminCreate1,"login",  "otherPwd", "someOtherSalt")
       } must throwAn[AccountAlreadyExistsException]
     }
 
     "retrieve salt" in new WithInMemoryDB {
-      await(AdminPostgreDAO.create(AdminCreate("firstName", "lastName", "admin@domain.com", DateTime.now), "login", "pwd", "someSalt"))
+      await(AdminPostgreDAO.create(Fixtures.adminCreate0, "login", "pwd", "someSalt"))
       await {
         AdminPostgreDAO.salt("login")
       }.get must be equalTo("someSalt")
     }
 
     "retrieve admin by login" in new WithInMemoryDB {
-      await(AdminPostgreDAO.create(AdminCreate("firstName", "lastName", "admin@domain.com", DateTime.now), "login", "pwd", "someSalt"))
+      await(AdminPostgreDAO.create(Fixtures.adminCreate0, "login", "pwd", "someSalt"))
       await {
         AdminPostgreDAO.byLogin("login")
       } must beSome
@@ -47,29 +44,36 @@ object AdminPostgreDaoSpec extends Specification with DaoSpec {
     }
 
     "authenticate an admin with its login and password" in new WithInMemoryDB {
-      await(AdminPostgreDAO.create(AdminCreate("firstName", "lastName", "admin@domain.com", DateTime.now), "login", "pwd", "someSalt"))
+      await(AdminPostgreDAO.create(Fixtures.adminCreate0, "login", "pwd", "someSalt"))
       await {
         AdminPostgreDAO.authenticate("login", "pwd")
       } must beSome
     }
 
-    // "retrieve by id" in new WithInMemoryDB {
-    //   val admin = await(AdminPostgreDAO.create(AdminCreate("firstName", "lastName", "admin@domain.com", DateTime.now), "login", "pwd", "someSalt"))
-    //   await {
-    //     AdminPostgreDAO.byId(admin.id)
-    //   } must beSome
-    // }
+    "retrieve by id" in new WithInMemoryDB {
+      val admin = await(AdminPostgreDAO.create(Fixtures.adminCreate0, "login", "pwd", "someSalt"))
+      await {
+        AdminPostgreDAO.byId(admin.id)
+      } must beSome
+    }
 
-    // "update the user" in new WithInMemoryDB {
-    //   val user = await(UserPostgreDAO.create(UserCreate("firstName", "lastName", "user@domain.com", DateTime.now), "login", "pwd", "someSalt"))
-    //   val newUser = await(UserPostgreDAO.update(user, UserUpdate("newFirstName", "newLastName", "user@newdomain.com")))
-    //   val readUser = await(UserPostgreDAO.byId(user.id)).get
+    "retrieve all admins" in new WithInMemoryDB {
+      await(AdminPostgreDAO.create(Fixtures.adminCreate0, "login", "pwd", "someSalt"))
+      await(AdminPostgreDAO.create(Fixtures.adminCreate1, "login1", "pwd", "someSalt"))
+      val admins = await(AdminPostgreDAO.all)
+      admins.length must be equalTo(2)
+    }
 
-    //   readUser.id.value must be equalTo(user.id.value)
-    //   readUser.firstName must be equalTo("newFirstName")
-    //   readUser.lastName must be equalTo("newLastName")
-    //   readUser.email must be equalTo("user@newdomain.com")
-    //   readUser.creation must be equalTo(user.creation)
-    // }
+    "update an admin" in new WithInMemoryDB {
+      val user = await(AdminPostgreDAO.create(Fixtures.adminCreate0, "login", "pwd", "someSalt"))
+      await(AdminPostgreDAO.update(user, Fixtures.adminUpdate1))
+      val readUser = await(AdminPostgreDAO.byId(user.id)).get
+
+      readUser.id.value must be equalTo(user.id.value)
+      readUser.firstName must be equalTo("firstName1")
+      readUser.lastName must be equalTo("lastName1")
+      readUser.email must be equalTo("admin1@domain.com")
+      readUser.creation must be equalTo(user.creation)
+    }
   }
 }
